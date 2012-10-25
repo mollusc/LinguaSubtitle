20f85f560f9cc023da13b7d09cfb6421ada2318a
mollusc <molluscLab@gmail.com>
2012-10-25 12:24:59 +0400

    Comment Speech

--- LinguaSubtitle/src/mollusc/linguasubtitle/subtitle/srt/Speech.java
+++ LinguaSubtitle/src/mollusc/linguasubtitle/subtitle/srt/Speech.java
@@ -1,8 +1,24 @@
 package mollusc.linguasubtitle.subtitle.srt;
 
+/**
+ * Container for data of a srt subtitle
+ * @author mollusc
+ *
+ */
 public class Speech {
+	/**
+	 * Sequence number of the speech
+	 */
 	public int sequenceNumber;
+	
+	/**
+	 * Timing of the speech
+	 */
 	public String timing;
+	
+	/**
+	 * Content of the speech
+	 */
 	public String content;
 
 	public Speech(int sequenceNumber, String timing, String content) {
