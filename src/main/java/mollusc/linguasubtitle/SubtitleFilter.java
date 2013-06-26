800e4b4b1dc12db5cce936b02542e00bc6aedba5
mollusc <molluscLab@gmail.com>
2013-06-26 11:52:11 +0400

    Migrate to Maven

---
+++ src/main/java/mollusc/linguasubtitle/SubtitleFilter.java
@@ -0,0 +1,32 @@
+package mollusc.linguasubtitle;
+
+import java.io.File;
+
+/**
+ * Filter subtitles
+ *
+ * @author mollusc <MolluscLab@gmail.com>
+ */
+public class SubtitleFilter extends javax.swing.filechooser.FileFilter {
+
+    String ext, description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    SubtitleFilter() {
+        this.ext = ".srt";
+        description = "Subtitles SRT";
+    }
+
+    public boolean accept(File f) {
+        if (f != null) {
+            if (f.isDirectory()) {
+                return true;
+            }
+            return f.toString().endsWith(ext);
+        }
+        return false;
+    }
+}
---
+++ src/main/java/mollusc/linguasubtitle/subtitle/srt/IndexWordComparator.java
@@ -0,0 +1,18 @@
+package mollusc.linguasubtitle.subtitle.srt;
+
+import java.util.Comparator;
+
+/**
+ * Sort IndexWord
+ *
+ * @author mollusc
+ */
+public class IndexWordComparator implements Comparator<IndexWord> {
+    @Override
+    public int compare(IndexWord arg0, IndexWord arg1) {
+        int compare = arg0.indexSpeech - arg1.indexSpeech;
+        if (compare == 0)
+            return arg1.start - arg0.start;
+        return compare;
+    }
+}
